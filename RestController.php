f7f43d7e6d360808ff9334ca10353ff9cad5283a
Selvin11 <528370630@qq.com>
2017-02-27 18:06:50 +0800

    put fix

--- RestController.php
+++ RestController.php
@@ -34,25 +34,34 @@ switch ($method) {
         break;
         
     case 'POST':
-        // 处理 REST Url  POST /site/list/
-        $siteRestHandler = new SiteRestHandler();
-        $siteRestHandler->postSite($_GET["title"],$_GET["content"]);
+         // 处理 REST URL  PUT /site/list/id/
+        if ($view == "single") {
+            $json = file_get_contents('php://input');
+            $data = json_decode($json,true);
+            var_dump($data);
+
+            $siteRestHandler = new SiteRestHandler();
+            if ($data == NULL) {
+                $siteRestHandler->putSite($_GET["id"],$_GET["title"],$_GET["content"]);
+            }else{
+                $siteRestHandler->putSite($data["id"],$data["title"],$data["content"]);
+            }
+        }else{
+            // 处理 REST Url  POST /site/list/
+            $siteRestHandler = new SiteRestHandler();
+            $siteRestHandler->postSite($_GET["title"],$_GET["content"]);
+        }
         break;
 
-    case 'PUT':
-        // 处理 REST URL  PUT /site/list/id/
-        if ($view == "single") {
-            $siteRestHandler = new SiteRestHandler();
-            $siteRestHandler->putSite($_GET["id"],$_GET["title"],$_GET["content"]);
-        }
-
+    
     case "DELETE":
         // 处理 REST URL  DELETE /site/list/id/
         if ($view == "single") {
             $siteRestHandler = new SiteRestHandler();
             $siteRestHandler->deleteSite($_GET["id"]);
         }
-
+        break;
+        
     case "" :
         //404 - not found;
         break;
